6fa8b4b5eb04e4fb2956f4c7f7739c7a337389d3
Farhan <muhfarhanr10@gmail.com>
2026-04-14 11:17:27 +0700

    change back

--- services/auth_service.py
+++ services/auth_service.py
@@ -1,15 +1,15 @@
 from typing import Optional
-from core.security import verify_password, create_access_token
-from schemas.user import UserLogin
+from surveyor_backend.core.security import verify_password, create_access_token
+from surveyor_backend.schemas.user import UserLogin
 
 # Hardcoded "Database"
 FAKE_USER_DB = {
     "admin": {
         "id": 1,
-        "username": "jaja",
+        "username": "admin",
         "full_name": "John Doe Surveyor",
         "email": "admin@example.com",
-        "hashed_password": "$2b$12$7kP.K6G06UvYw.v7tXN3Se6nI6yZ9vLzS8R4iX.O/2/29P5g7B2m2", # hash for 'jaja', # hash for 'jakarta'  # hash for 'password123'
+        "hashed_password": "$2b$12$tDi4ojWGSssQBVHts4Gp4Ohi8Ew/2uauMZs6iYuxspe/.LUGhhA0i",  # hash for 'password123'
         "is_active": True,
     }
 }
